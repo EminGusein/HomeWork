08e91a9c4d07043e45256f9cce29462b175f2f08
Emin <krates01@gmail.com>
2023-07-06 22:34:06 +0300

    Sdelal zadachu 8

--- Zadacha8/Program.cs
+++ Zadacha8/Program.cs
@@ -2,3 +2,9 @@
 
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
+int counter = 2;
+while(counter <= N)
+{
+    Console.Write(counter + " ");
+    counter = counter + 2;
+}
